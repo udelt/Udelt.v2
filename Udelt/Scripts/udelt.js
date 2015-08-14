3a1536048b448614db2c0e8c11f6d8cbcb9b2608
Dag Helge Østerhagen <daghelge@gmail.com>
2015-08-14 14:44:20 +0200

    Last checkin before removing fixed

--- Udelt/Scripts/udelt.js
+++ Udelt/Scripts/udelt.js
@@ -1,55 +1,36 @@
 ﻿
-$(function() {
+$(function () {
 
     new WOW().init();
 
-    //$('#what-scene').parallax({
-    //    limitY: 1,
-    //    limitX: 25,
-    //    scalarX: 5
-    //});
-
-    //$('#who-scene').parallax();
-
-
     function createSplines() {
         var width = $(window).width();
-        var canvas = document.getElementById("topSpline");
-        //var ctx = canvas.getContext("2d")
-        //ctx.lineWidth = 2;
-        //ctx.strokeStyle = "#333";
-        //ctx.beginPath();
-        //ctx.moveTo(0, 100);
-        //ctx.quadraticCurveTo(50, 0, 400, 0);
-        //ctx.stroke();
 
-        $('#topSpline').drawEllipse({
-            fillStyle: '#FEFEFA',
-            x: 150, y: 150,
-            width: width+100, height: 200,
-            strokeStyle: '#333',
-            strokeWidth: 4,
-        });
+        $('#topSpline')
+            .clearCanvas()
+            .drawEllipse({
+                fillStyle: '#FEFEFA',
+                x: 150, y: 150,
+                width: width, height: 200,
+                strokeStyle: '#333',
+                strokeWidth: 4,
+                rotate: 10
+            });
 
-        console.log("Spline created, width " + width);
+        $('#bottomSpline')
+            .clearCanvas()
+            .drawEllipse({
+                fillStyle: '#FEFEFA',
+                x: 0, y: 0,
+                width: width * 1.5, height: 200,
+                strokeStyle: '#333',
+                strokeWidth: 4,
+                rotate: 190
+            });
+
+
 
     }
-
-    function disableClickOnScroll() {
-        var body = document.body, timer;
-
-        window.addEventListener('scroll', function () {
-            clearTimeout(timer);
-            if (!body.classList.contains('disable-hover')) {
-                body.classList.add('disable-hover')
-            }
-
-            timer = setTimeout(function () {
-                body.classList.remove('disable-hover')
-            }, 500);
-        }, false);
-    }
-
 
     function adjustRightTree(st) {
         var el = $('#tree-right-img')
@@ -76,25 +57,120 @@ $(function() {
         }
     }
 
-  
+
+    function setHeights() {
+        var whoContentHeight = $("#who-content").height();
+        var splineHeight = 50;
+
+        $("#who-overlay").height(whoContentHeight - splineHeight);
+        //console.log("who-overlay height:  " + (whoContentHeight - splineHeight));
+
+        var whoHeight = $("#who").height();
+        $("#footer").height(whoContentHeight * 10);
+        console.log("who height:  " + whoContentHeight * 1.2);
+
+
+    }
+
+    function velHide(el) {
+        el.velocity(
+              {
+                  opacity: 0
+              },
+              {
+                  duration: 0,
+                  display: "none"
+              });;
+    }
+
+    function velShow(el) {
+        el.velocity(
+              {
+                  opacity: 1
+              },
+              {
+                  duration: 0,
+                  display: "block"
+              });;
+    }
+
+
+    $(window).resize(function (event) {
+        setHeights();
+        createSplines();
+    });
+
+    var prevSt = 0;
     $(window).scroll(function (event) {
         var st = $(this).scrollTop();
 
+
         adjustRightTree(st);
 
-        if (st > 700) {
-            $("#what").addClass('hidden');
-            $("#where").removeClass('hidden');
+        var whatHeight = $("#what").height();
 
-        } else {
-            $("#what").removeClass('hidden');
-            $("#where").addClass('hidden');
+        if (st > whatHeight + 80 & prevSt <= whatHeight + 80) {
+            velHide($("#what"));
+            velShow($("#where"));
+        } else if ((st <= whatHeight + 80 & prevSt > whatHeight + 80)) {
+            velShow($("#what"));
+            velHide($("#where"));
         }
+
+        prevSt = st;
 
     });
 
     createSplines();
-    disableClickOnScroll();
+    setHeights();
+    animateDiv();
+
+
+
+
+    function makeNewPosition($container) {
+
+        // Get viewport dimensions (remove the dimension of the div)
+        $container = ($container || $(window))
+        var h = $container.height() - 50;
+        var w = $container.width() - 50;
+
+        var nh = Math.floor(Math.random() * h);
+        var nw = Math.floor(Math.random() * w);
+
+        return [nh, nw];
+
+    }
+
+    function animateDiv() {
+        var $target = $('.move-me');
+        console.log($target);
+        var newq = makeNewPosition($target.parent());
+        var oldq = $target.offset();
+        var speed = calcSpeed([oldq.top, oldq.left], newq);
+
+        $('.move-me').velocity({
+            top: newq[0],
+            left: newq[1]
+        }, speed, function () {
+            animateDiv();
+        });
+
+    };
+
+    function calcSpeed(prev, next) {
+
+        var x = Math.abs(prev[1] - next[1]);
+        var y = Math.abs(prev[0] - next[0]);
+
+        var greatest = x > y ? x : y;
+
+        var speedModifier = 0.1;
+
+        var speed = Math.ceil(greatest / speedModifier);
+
+        return speed;
+    }
 
 
 });
